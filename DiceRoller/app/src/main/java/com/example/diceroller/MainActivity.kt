263d7f60e5f8d4056201009bf443fd6ddf515bf8
wjdtj9656 <wjdtj9656@naver.com>
2021-10-08 15:18:37 +0900

    dice roller example

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -21,17 +22,29 @@ class MainActivity : AppCompatActivity() {
           // val toast = Toast.makeText(this,"Dice Rolled",Toast.LENGTH_SHORT)
             // toast.show()
 
-           // Toast.makeText(this,"Dice Rolled!!",Toast.LENGTH_SHORT).show()
-        //val resultTextView: TextView = findViewById(R.id.textView)
-         //   resultTextView.text = "6"
+             // Toast.makeText(this,"Dice Rolled!!",Toast.LENGTH_SHORT).show()
+            // val resultTextView: TextView = findViewById(R.id.textView)
+            //   resultTextView.text = "6"
             rollDice()
         }
+        rollDice()
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+       // val resultTextView: TextView = findViewById(R.id.textView)
+       // resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when(diceRoll){
+            1 -> (R.drawable.dice_1)
+            2 -> (R.drawable.dice_2)
+            3 -> (R.drawable.dice_3)
+            4 -> (R.drawable.dice_4)
+            5 -> (R.drawable.dice_5)
+            else -> (R.drawable.dice_6)
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
